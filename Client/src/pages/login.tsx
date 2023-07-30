da691d67fbf896267ebe83462c7c7d390cac86b5
Ghufran <ghufranshahid101@gmail.com>
2023-07-30 17:31:16 -0400

    Added size styling for wrapper div surrounding login component

--- Client/src/pages/login.tsx
+++ Client/src/pages/login.tsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom"
 
 function Login() {
   return (
-    <div>
+    <div className="min-h-screen min-w-[100vw]">
       <form action="" method="" role="form">
         <h1>Log In.</h1>
       </form>
