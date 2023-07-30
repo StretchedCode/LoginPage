4ed8f83bacd462331d898a2616a747b3a39df33a
Ghufran <ghufranshahid101@gmail.com>
2023-07-29 23:56:40 -0400

    Alterred heading text to match test assertion

--- Client/src/pages/login.tsx
+++ Client/src/pages/login.tsx
@@ -3,7 +3,9 @@ import { Routes, Route } from "react-router-dom"
 function Login() {
   return (
     <>
-      <form action="" method="" role="form"></form>
+      <form action="" method="" role="form">
+        <h1>Log In.</h1>
+      </form>
     </>
   )
 }
