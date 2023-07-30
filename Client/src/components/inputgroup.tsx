9f68ebb888cca458b99c4991c1e737be2166bdeb
Ghufran <ghufranshahid101@gmail.com>
2023-07-30 18:19:27 -0400

    Created base component for input group without any functional state

--- Client/src/components/inputgroup.tsx
+++ Client/src/components/inputgroup.tsx
@@ -2,13 +2,25 @@ import { FC } from "react"
 
 interface inputGroupProps {
   dataLabel: string
-  labelRole?: string
-  inputRole?: string
+  labelRole?: string | " "
+  inputRole?: string | " "
   inputType: string
+  inputPlaceholder?: string | " "
 }
 
 const InputGroup: FC<inputGroupProps> = (props) => {
-  return <div>Test group</div>
+  return (
+    <div>
+      <label htmlFor={props.dataLabel} role={props.labelRole}></label>
+      <input
+        type={props.inputType}
+        name={props.dataLabel}
+        role={props.inputRole}
+        required
+        placeholder={props.inputPlaceholder}
+      ></input>
+    </div>
+  )
 }
 
 export default InputGroup
