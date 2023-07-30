dfe8f3b266934a622e719ae6b118e6804ede3122
Ghufran <ghufranshahid101@gmail.com>
2023-07-30 00:35:17 -0400

    Changed HTML fragment to div for styling purposes

--- Client/src/pages/login.tsx
+++ Client/src/pages/login.tsx
@@ -2,11 +2,11 @@ import { Routes, Route } from "react-router-dom"
 
 function Login() {
   return (
-    <>
+    <div>
       <form action="" method="" role="form">
         <h1>Log In.</h1>
       </form>
-    </>
+    </div>
   )
 }
 
