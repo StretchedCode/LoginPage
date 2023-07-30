718411ec78707c03c723bd32dd4c3951bdb51357
Ghufran <ghufranshahid101@gmail.com>
2023-07-29 23:43:44 -0400

    Removed redundant test for render of entire element, changed form render test title

--- Client/src/tests/login.test.tsx
+++ Client/src/tests/login.test.tsx
@@ -2,13 +2,7 @@ import Login from "../pages/login"
 import { render, screen } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 
-test("render of login page", () => {
-  const { container } = render(<Login></Login>)
-
-  expect(screen.getByRole("label").textContent).toMatch(/Log in/i)
-})
-
-test("Ensuring render of Form", () => {
+test("render of Form", () => {
   const { container } = render(<Login></Login>)
 
   expect(screen.getByRole("form")).toMatchSnapshot()
