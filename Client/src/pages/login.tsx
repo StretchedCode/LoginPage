6b5667ee725482d1a1f60a647cf2c16d37771103
Ghufran <ghufranshahid101@gmail.com>
2023-07-30 18:26:05 -0400

    Added import for inputgroup component, inserted components into form element

--- Client/src/pages/login.tsx
+++ Client/src/pages/login.tsx
@@ -1,23 +1,25 @@
 import { Routes, Route } from "react-router-dom"
+import InputGroup from "../components/inputgroup"
 
 function Login() {
   return (
     <div className="min-h-screen min-w-[100vw] bg-red-900 flex flex-col p-5 items-center md:flex-row md:justify-between md:p-10">
-      <form action="" method="" className="bg-white p-5 rounded">
+      <form action="" method="" role="form" className="bg-white p-5 rounded">
         <h1>Log In.</h1>
-        <div>
-          <label
-            role="user-label"
-            aria-label="username input"
-            htmlFor="username"
-          ></label>
-          <input
-            type="text"
-            required
-            placeholder="Username ex. Joe123"
-            name="username"
-          ></input>
-        </div>
+        <InputGroup
+          dataLabel="username"
+          labelRole="user-label"
+          inputType="text"
+          inputRole="username-input"
+          inputPlaceholder="Username ex. Joe123"
+        ></InputGroup>
+        <InputGroup
+          dataLabel="password"
+          labelRole="password-label"
+          inputType="password"
+          inputRole="user-password"
+          inputPlaceholder="password"
+        ></InputGroup>
       </form>
     </div>
   )
