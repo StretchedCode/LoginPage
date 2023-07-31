838f35f92442282aab3fc9ae9e52ffa1e50f71e2
Ghufran <ghufranshahid101@gmail.com>
2023-07-31 09:16:05 -0400

    Added colour and outline styling to circle div

--- Client/src/components/loginCircles.tsx
+++ Client/src/components/loginCircles.tsx
@@ -1,6 +1,6 @@
 function LoginCircles() {
   return (
-    <div className="h-9 w-9 rounded-full bg-yellow-400 absolute -z-10"></div>
+    <div className="h-16 w-16 rounded-full bg-yellow-400 absolute outline outline-yellow-400 outline-offset-[30px] bottom-0 left-0"></div>
   )
 }
 
