f02dd0ebbe686d882980f68e7a52187f3822226d
Ghufran <ghufranshahid101@gmail.com>
2023-07-31 12:19:16 -0400

    Added value and function in order to control value of inputs, alongside providing the data for form verification within login

--- Client/src/components/inputgroup.tsx
+++ Client/src/components/inputgroup.tsx
@@ -6,6 +6,8 @@ interface inputGroupProps {
   inputRole?: string | " "
   inputType: string
   inputPlaceholder?: string | " "
+  value: string
+  onChange(e: any): void
 }
 
 const InputGroup: FC<inputGroupProps> = (props) => {
@@ -18,6 +20,8 @@ const InputGroup: FC<inputGroupProps> = (props) => {
         role={props.inputRole}
         required
         placeholder={props.inputPlaceholder}
+        value={props.value}
+        onChange={props.onChange}
         className="min-w-full min-h-[10%] bg-slate-200 focus:outline-none p-3 placeholder:text-slate-500"
       ></input>
     </div>
