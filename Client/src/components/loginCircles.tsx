776a9192b6282b956315b071f51a4fbde7ce2133
Ghufran <ghufranshahid101@gmail.com>
2023-07-31 09:20:06 -0400

    Added two more circles with alterred outline offset and size values

--- Client/src/components/loginCircles.tsx
+++ Client/src/components/loginCircles.tsx
@@ -1,6 +1,10 @@
 function LoginCircles() {
   return (
-    <div className="h-16 w-16 rounded-full bg-yellow-400 absolute outline outline-yellow-400 outline-offset-[30px] bottom-0 left-0"></div>
+    <>
+      <div className="h-16 w-16 rounded-full bg-yellow-400 absolute outline outline-yellow-400 outline-offset-[30px] bottom-6 left-12"></div>
+      <div className="h-24 w-24 rounded-full bg-yellow-400 absolute outline outline-yellow-400 outline-offset-[80px] top-24 right-16"></div>
+      <div className="h-32 w-32 rounded-full bg-yellow-400 absolute outline outline-yellow-400 outline-offset-[100px] top-4 left-12"></div>
+    </>
   )
 }
 
