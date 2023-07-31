9a5a604179d6aa6f8d79e790e17c37bdc655f1ee
Ghufran <ghufranshahid101@gmail.com>
2023-07-31 09:10:43 -0400

    Added absolute positioning to test element, alterred z-index

--- Client/src/components/loginCircles.tsx
+++ Client/src/components/loginCircles.tsx
@@ -1,5 +1,7 @@
 function LoginCircles() {
-  return <div className="h-9 w-9 rounded-full bg-yellow-400"></div>
+  return (
+    <div className="h-9 w-9 rounded-full bg-yellow-400 absolute -z-10"></div>
+  )
 }
 
 export default LoginCircles
