2e71f2ae18abf540c442b0a9baf2f8dbbdf320ae
Ghufran <ghufranshahid101@gmail.com>
2023-07-31 12:05:35 -0400

    Added checkForm function to validate inputs and send request to API endpoint, removed action and method attributes from form.

--- Client/src/pages/login.tsx
+++ Client/src/pages/login.tsx
@@ -1,14 +1,30 @@
 import { Routes, Route } from "react-router-dom"
 import InputGroup from "../components/inputgroup"
 import LoginCircles from "../components/loginCircles"
+import { FormEvent, useState } from "react"
 
 function Login() {
+  const [username, setUsername] = useState("test")
+  const [password, setPassword] = useState("pass")
+  const [validForm, setValidForm] = useState(true)
+
+  const checkForm = (e: FormEvent) => {
+    if (username === "" || password === "")
+      setValidForm((validForm) => !validForm)
+    else
+      fetch("http://localhost:3000/log-in", {
+        method: "POST",
+        mode: "cors",
+        body: JSON.stringify([username, password]),
+      })
+    e.preventDefault()
+  }
+
   return (
     <div className="min-h-screen min-w-[100vw] bg-red-900 flex flex-col p-5 justify-center items-center xl:flex-row xl:justify-between xl:p-10">
       <LoginCircles></LoginCircles>
       <form
-        action="/"
-        method="post"
+        onSubmit={checkForm}
         role="form"
         className="bg-white z-10 p-7 rounded flex flex-col gap-5 min-w-[30%]"
       >
@@ -32,6 +48,7 @@ function Login() {
           aria-label="form submit button"
           formAction="submit"
           className="bg-yellow-400 rounded-md font-semibold p-3 text-md hover:bg-yellow-500"
+          type="submit"
         >
           Log in
         </button>
