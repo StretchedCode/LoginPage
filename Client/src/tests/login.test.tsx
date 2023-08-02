443fba1e1694ee2f6be1aa601556e5252f05b177
Ghufran <ghufranshahid101@gmail.com>
2023-08-01 20:57:05 -0400

    Added prop definitions to each test

--- Client/src/tests/login.test.tsx
+++ Client/src/tests/login.test.tsx
@@ -4,50 +4,50 @@ import userEvent from "@testing-library/user-event"
 
 describe("Form testing", () => {
   test("Testing header label", () => {
-    const { container } = render(<Login></Login>)
+    const { container } = render(<Login type="log-in" apiUrl="login"></Login>)
 
     expect(screen.getByRole("heading").textContent).toMatch("Log in")
   })
 
   test("Testing for user label", () => {
-    const { container } = render(<Login></Login>)
+    const { container } = render(<Login type="log-in" apiUrl="login"></Login>)
 
     expect(screen.getByRole("user-label").textContent).toMatch("")
   })
 
   test("Testing input for username", () => {
-    const { container } = render(<Login></Login>)
+    const { container } = render(<Login type="log-in" apiUrl="login"></Login>)
 
     expect(screen.getByPlaceholderText("Username").textContent).toMatch("")
   })
 
   test("Testing for password label", () => {
-    const { container } = render(<Login></Login>)
+    const { container } = render(<Login type="log-in" apiUrl="login"></Login>)
 
     expect(screen.getByRole("password-label").textContent).toMatch("")
   })
 
   test("Testing for password input", () => {
-    const { container } = render(<Login></Login>)
+    const { container } = render(<Login type="log-in" apiUrl="login"></Login>)
 
     expect(screen.getByPlaceholderText("Password").textContent).toMatch("")
   })
 
   test("Valid Login Button", () => {
-    const { container } = render(<Login></Login>)
+    const { container } = render(<Login type="log-in" apiUrl="login"></Login>)
 
     expect(screen.getByRole("submit-login").textContent).toMatch("Log in")
   })
 
   test("Sign-up button", () => {
-    const { container } = render(<Login></Login>)
+    const { container } = render(<Login type="log-in" apiUrl="login"></Login>)
 
     expect(screen.getByRole("sign-up").textContent).toMatch("Sign up")
   })
 })
 
 test("DOM structure matches snapshot", () => {
-  const { container } = render(<Login></Login>)
+  const { container } = render(<Login type="log-in" apiUrl="login"></Login>)
 
   expect(container).toMatchSnapshot()
 })
