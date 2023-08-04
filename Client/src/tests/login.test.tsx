85be55676dc7b14ae07641cc93fdd223c13b2cc3
Ghufran <ghufranshahid101@gmail.com>
2023-08-03 22:21:30 -0400

    Updated reference to Form component, added BrowserRouter wrapper to all tests in order to avoid Navigate hook errors

--- Client/src/tests/login.test.tsx
+++ Client/src/tests/login.test.tsx
@@ -1,53 +1,69 @@
-import Login from "../pages/login"
+import Form from "../pages/login"
 import { render, screen } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
+import { BrowserRouter } from "react-router-dom"
 
-describe("Form testing", () => {
+describe("Form testing - log-in", () => {
   test("Testing header label", () => {
-    const { container } = render(<Login type="log-in" apiUrl="login"></Login>)
+    const { container } = render(<Form type="log-in" apiUrl="login"></Form>, {
+      wrapper: BrowserRouter,
+    })
 
     expect(screen.getByRole("heading").textContent).toMatch("Log in")
   })
 
   test("Testing for user label", () => {
-    const { container } = render(<Login type="log-in" apiUrl="login"></Login>)
+    const { container } = render(<Form type="log-in" apiUrl="login"></Form>, {
+      wrapper: BrowserRouter,
+    })
 
     expect(screen.getByRole("user-label").textContent).toMatch("")
   })
 
   test("Testing input for username", () => {
-    const { container } = render(<Login type="log-in" apiUrl="login"></Login>)
-
+    const { container } = render(<Form type="log-in" apiUrl="login"></Form>, {
+      wrapper: BrowserRouter,
+    })
     expect(screen.getByPlaceholderText("Username").textContent).toMatch("")
   })
 
   test("Testing for password label", () => {
-    const { container } = render(<Login type="log-in" apiUrl="login"></Login>)
+    const { container } = render(<Form type="log-in" apiUrl="login"></Form>, {
+      wrapper: BrowserRouter,
+    })
 
     expect(screen.getByRole("password-label").textContent).toMatch("")
   })
 
   test("Testing for password input", () => {
-    const { container } = render(<Login type="log-in" apiUrl="login"></Login>)
+    const { container } = render(<Form type="log-in" apiUrl="login"></Form>, {
+      wrapper: BrowserRouter,
+    })
 
     expect(screen.getByPlaceholderText("Password").textContent).toMatch("")
   })
 
-  test("Valid Login Button", () => {
-    const { container } = render(<Login type="log-in" apiUrl="login"></Login>)
+  test("Valid Form Button", () => {
+    const { container } = render(<Form type="log-in" apiUrl="login"></Form>, {
+      wrapper: BrowserRouter,
+    })
 
     expect(screen.getByRole("submit-login").textContent).toMatch("Log in")
   })
 
   test("Sign-up button", () => {
-    const { container } = render(<Login type="log-in" apiUrl="login"></Login>)
+    const { container } = render(<Form type="log-in" apiUrl="login"></Form>, {
+      wrapper: BrowserRouter,
+    })
 
     expect(screen.getByRole("sign-up").textContent).toMatch("Sign up")
   })
 })
 
 test("DOM structure matches snapshot", () => {
-  const { container } = render(<Login type="log-in" apiUrl="login"></Login>)
+  const { container } = render(<Form type="log-in" apiUrl="login"></Form>, {
+    wrapper: BrowserRouter,
+  })
 
   expect(container).toMatchSnapshot()
 })
