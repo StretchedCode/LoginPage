b309349efe94c49490f6c851294cda49d791117f
StretchedCode <ghufranshahid101@gmail.com>
2023-08-09 06:53:15 -0400

    Added breadcrumb component, to show status of fetch requests

---
+++ Client/src/components/breadcrumb.tsx
@@ -0,0 +1,20 @@
+
+
+
+interface CrumbProps {
+    text: string,
+    status: "success" | "error"
+}
+
+
+function BreadCrumb(props: CrumbProps){
+
+
+    return (
+        <div>
+            hello
+        </div>
+    )
+}
+
+export default BreadCrumb
