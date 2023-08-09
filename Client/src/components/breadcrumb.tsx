72f81b425d563727a08132501445fce9b9e1968f
StretchedCode <ghufranshahid101@gmail.com>
2023-08-09 07:06:35 -0400

    Separated props from one another in divs, adjusted styling to match tiered layout of content

--- Client/src/components/breadcrumb.tsx
+++ Client/src/components/breadcrumb.tsx
@@ -11,8 +11,9 @@ function BreadCrumb(props: CrumbProps){
 
 
     return (
-        <div>
-            hello
+        <div className="flex flex-col p-4 rounded-md shadow-sm self-end xl:absolute xl:bottom-5 xl:right-5 bg-white">
+            <div className="pb-1 font-semibold text-xl border-b border-slate-600">{props.status}</div>
+            <div className="pt-2">{props.text}</div>
         </div>
     )
 }
