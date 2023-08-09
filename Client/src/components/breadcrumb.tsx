91656cd908f29da351cc2a92a3aa4c6f9e9b75e3
StretchedCode <ghufranshahid101@gmail.com>
2023-08-09 07:27:06 -0400

    Added state to control visibility of breadcrumb element

--- Client/src/components/breadcrumb.tsx
+++ Client/src/components/breadcrumb.tsx
@@ -1,5 +1,5 @@
 
-
+import { useState } from "react"
 
 interface CrumbProps {
     text: string,
@@ -9,9 +9,13 @@ interface CrumbProps {
 
 function BreadCrumb(props: CrumbProps){
 
+    const [visible, setVisible] = useState(true)
 
     return (
-        <div className="flex flex-col p-4 rounded-md shadow-sm self-end xl:absolute xl:bottom-5 xl:right-5 bg-white">
+        <div className={visible ? "flex flex-col p-4 rounded-md shadow-sm self-center mt-10 absolute bottom-5 right-5 bg-white" : "hidden"}>
+            <button className="absolute top-2 right-2 outline outline-1 rounded-full w-5 h-5 text-sm font-semibold hover:bg-black hover:text-white"
+            onClick={() => {setVisible(visible => false)}}
+            >x</button>
             <div className="pb-1 font-semibold text-xl border-b border-slate-600">{props.status}</div>
             <div className="pt-2">{props.text}</div>
         </div>
