3b72e29bc2adc8f7c87b81ac4b4973ef755edcf2
StretchedCode <ghufranshahid101@gmail.com>
2023-08-09 08:06:14 -0400

    Added userReducer to store

--- Client/src/app/store.ts
+++ Client/src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit"
 import counterReducer from "../slices/counterSlice"
+import userReducer from "../slices/userSlice"
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    User: userReducer
   },
 })
 
